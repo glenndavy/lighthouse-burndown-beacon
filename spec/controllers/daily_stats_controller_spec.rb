e2aea7d410660e5c121e35b4b5141b7db67a05c7
Glenn Davy <glenn@davy.net.au>
2008-10-26 02:40:24 +1100

    fixed some tests

--- spec/controllers/daily_stats_controller_spec.rb
+++ spec/controllers/daily_stats_controller_spec.rb
@@ -2,15 +2,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe DailyStatsController do
 
+  before(:each) do
+    DailyStat.stub!(:count).and_return(5)
+    DailyStat.stub!(:most_recent_collection_date).and_return(Date.new)
+    DailyStat.stub!(:collected_period_as_sentence).and_return("abcde")
+  end
+ 
+
   it "should do index" do
     @stats = [] << @stat = mock('daily_stat')
     DailyStat.stub!(:find).and_return(@stats)
     DailyStat.should_receive(:paginate).and_return(@stats)
     get :index
     response.should be_success
-    assigns(:stat_count).should be_number
-    assigns(:latest_stat_message).should be_string
-    assigns(:collection_period_message).should be_string 
+    assigns(:stat_count).class.name.should == "Fixnum"
+    assigns(:latest_stat_message).class.name.should == "String"
+    assigns(:collection_period_message).class.name.should == "String"
   end
 
   it "should harvest" do
@@ -19,12 +26,18 @@ describe DailyStatsController do
     get :harvest
     response.should redirect_to("/daily-stats/index") 
     flash[:notice].should_not be_nil
+    assigns(:stat_count).class.name.should == "Fixnum"
+    assigns(:latest_stat_message).class.name.should == "String"
+    assigns(:collection_period_message).class.name.should == "String"
   end
 
 
   it "should do reports" do
     get "reports"
-    response.should be_success
+    response.should be_redirect #for now
+    assigns(:stat_count).class.name.should == "Fixnum"
+    assigns(:latest_stat_message).class.name.should == "String"
+    assigns(:collection_period_message).class.name.should == "String"
   end
 end
 
